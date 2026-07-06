68c1afdeb001bfbd440283dca13cdf683610a750
anthony <info@anthonycandaele.be>
2026-07-06 13:54:07 +0000

    implement css design

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,8 +1,19 @@
 class Budget < ApplicationRecord
   has_many :transactions, dependent: :destroy
-  
+
   def formatted_month
     month.strftime("%B %Y")
   end
 
+  def total_income
+    transactions.income.sum(:amount)
+  end
+
+  def total_expenses
+    transactions.expense.sum(:amount)
+  end
+
+  def remaining_balance
+    total_income - total_expenses
+  end
 end
